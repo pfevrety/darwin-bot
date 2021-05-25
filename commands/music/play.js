78748501002990bf59d5da7c29d331f9cc0bbd04
Fevret <60936445+pfevrety@users.noreply.github.com>
2021-05-25 20:14:42 +0200

    🐛Fix bugs

--- commands/music/play.js
+++ commands/music/play.js
@@ -1,8 +1,15 @@
 module.exports = {
     name: 'play',
-    aliases: ['music'],
-    description: 'Get the ping of the bot',
-    execute(message, args, distube) {
-        distube.play(message, args.join(' '))
+    aliases: ['music', 'p'],
+    usage: '<url>',
+    async execute(message, args, distube) {
+        const string = args.join(" ")
+        if (!string) return message.channel.send(`Error | Please enter a song url or query to search.`)
+        try {
+            return distube.play(message, string)
+        } catch (e) {
+            console.log("e")
+            return message.reply(`\`\`${e}\`\``)
+        }
     },
 };
