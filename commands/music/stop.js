028e39a4d871018571f5c7c538029439f64011ee
Fevret <60936445+pfevrety@users.noreply.github.com>
2021-05-25 20:15:16 +0200

    Add stop

--- commands/music/stop.js
+++ commands/music/stop.js
@@ -4,5 +4,7 @@ module.exports = {
     description: 'Get the ping of the bot',
     execute(message, args, distube) {
         distube.stop(message, args.join(' '))
+        distube.stop(message);
+        message.channel.send("Stopped the queue!");
     },
 };
