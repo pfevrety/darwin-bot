4e8f5a3f1692b7860e05221158115de744024f45
Fevret <60936445+pfevrety@users.noreply.github.com>
2021-05-26 16:13:48 +0200

    ✨Add language support to autoplay

--- commands/music/autoplay.js
+++ commands/music/autoplay.js
@@ -1,13 +1,12 @@
+const language = require('../../middleware/language')
+
 module.exports = {
     name: 'autoplay',
     usage: 'ON || OFF',
     description: 'Active autoplay',
     async execute(message, args, distube) {
         let mode = distube.toggleAutoplay(args.join(" "));
-        try {
-            message.channel.send("Set autoplay mode to `" + (mode ? "On" : "Off") + "`");
-        } catch (e) {
-            message.reply("``" + e + "``")
-        }
+        message.channel.send(`${language(message.guild, "MUSIC_AUTOPLAY_SET")} \`\`${(mode ? "On" : "Off")}\`\``);
+
     },
 };
