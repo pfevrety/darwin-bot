7934e09ec85c466d069aa9b77d8962a5401a3aa3
Fevret <60936445+pfevrety@users.noreply.github.com>
2021-05-26 16:13:56 +0200

    ✨Add language support to jump

--- commands/music/jump.js
+++ commands/music/jump.js
@@ -1,10 +1,14 @@
+const language = require('../../middleware/language')
+
 module.exports = {
     name: 'jump',
     aliases: ['j'],
     description: 'Get the ping of the bot',
     async execute(message, args, distube) {
-        distube.jump(message, parseInt(args[0]))
-            .catch(err => message.channel.send("Invalid song number."));
-
+        try{
+            distube.jump(message, parseInt(args[0])).then(message.channel.send(`${language(message.guild, "MUSIC_JUMP_SUCCEED")}`))
+        } catch (e) {
+            message.channel.send(`${language(message.guild, "MUSIC_JUMP_INVALID_SONG")}`)
+        }
     },
 };
