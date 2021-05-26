0cb91ab2ed7e4e3505e2197b433096b17efaac6b
Fevret <60936445+pfevrety@users.noreply.github.com>
2021-05-26 16:14:43 +0200

    ✨Add language support to skip

--- commands/music/skip.js
+++ commands/music/skip.js
@@ -1,8 +1,11 @@
+const language = require('../../middleware/language');
+
 module.exports = {
     name: 'skip',
     aliases: ['next'],
     description: 'Get the ping of the bot',
     execute: function (message, args, distube) {
         distube.skip(message);
+        return message.reply(language(message.guild, "COMMAND_SUCCESS"))
     },
 };
