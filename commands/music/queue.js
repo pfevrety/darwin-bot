8ade1a72c9f801d3bd588e77d4d6255c093947e0
Fevret <60936445+pfevrety@users.noreply.github.com>
2021-05-26 16:14:14 +0200

    ✨Add language support to queue

--- commands/music/queue.js
+++ commands/music/queue.js
@@ -1,9 +1,11 @@
+const language = require('../../middleware/language');
+
 module.exports = {
     name: 'queue',
     aliases: ['queu', 'q', 'getqueue'],
     async execute(message, args, distube) {
         let queue = distube.getQueue(message);
-        message.channel.send('Current queue:\n' + queue.songs.map((song, id) =>
+        message.channel.send(language(message.guild, "MUSIC_QUEUE_CURRENT") + queue.songs.map((song, id) =>
             `**${id + 1}**. [${song.name}](${song.url}) - \`${song.formattedDuration}\``
         ).join("\n"));
 
