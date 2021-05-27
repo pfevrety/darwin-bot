ed17a240db2ead37572a7e6c737fb3b019b293b2
Fevret <60936445+pfevrety@users.noreply.github.com>
2021-05-27 18:27:46 +0200

    🐛Fix ready event command

--- events/client/ready.js
+++ events/client/ready.js
@@ -2,6 +2,6 @@ module.exports = {
     name: 'ready',
     once: true,
     execute(client) {
-        client.channels.cache.get(`836331037712711690`).send(`:gear: Système: Démarrage terminé ! ✅`)
+        // client.channels.cache.get(`836331037712711690`).send(`:gear: Système: Démarrage terminé ! ✅`)
     },
 };
