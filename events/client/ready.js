2887d57367caade0f2f6bf312afa809450b343c6
Fevret <60936445+pfevrety@users.noreply.github.com>
2021-07-19 13:33:04 +0200

    Delete ready.js

--- events/client/ready.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    name: 'ready',
-    once: true,
-    execute(client) {
-        // client.channels.cache.get(`836331037712711690`).send(`:gear: Système: Démarrage terminé ! ✅`)
-    },
-};
