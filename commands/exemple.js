fb7573f0783ec3f09686b462de3585571982d96f
Fevret <60936445+pfevrety@users.noreply.github.com>
2021-07-19 13:32:43 +0200

    Create exemple.js

---
+++ commands/exemple.js
@@ -0,0 +1,13 @@
+module.exports = {
+    name: 'A command',
+    description: 'A description',
+    aliases: ['Aliases'],
+    usage: '<example>',
+    permissions: ['MANAGE_GUILD'],
+    cooldown: 15,
+    args: true,
+    owner: false,
+    execute(message, args) {
+
+    },
+};
