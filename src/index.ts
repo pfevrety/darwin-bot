6bf4059c9d41ba86cbfd34ed5d8b9480e19a244e
Fevret <efevret@gmail.Com>
2021-08-11 12:29:37 +0200

    ✨create Index.ts

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import { SapphireClient } from '@sapphire/framework';
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+const client = new SapphireClient({ intents: ['GUILDS', 'GUILD_MESSAGES'] });
+
+client.fetchPrefix = () => '!';
+
+client.login(process.env.TOKEN).then(() => console.log(client)).catch(console.error);
