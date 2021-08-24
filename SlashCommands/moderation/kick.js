25b27cb7dfd53d787e4a66e4c226f138319d5f95
Fevret <efevret@gmail.com>
2021-08-24 13:17:54 +0200

    ✨ Add / command

--- SlashCommands/moderation/kick.js
+++ SlashCommands/moderation/kick.js
@@ -6,19 +6,36 @@ const {
 
 module.exports = {
     name: "kick",
-    description: "kick someone",
-    type: "CHAT_INPUT",
+    type: 1,
+    description: "Send a random adorable animal photo",
+    options: [
+        {
+            name: "target",
+            description: "The kick target",
+            type: 6,
+            required: true,
+            choices: []
+        },
+        {
+            name: "reason",
+            description: "The reason of the kick",
+            type: 3,
+            required: false,
+            choices: []
+        }
+    ],
+
     /**
      *
      * @param {CommandInteraction} interaction
      * @param {String[]} args
      */
     run: async (interaction, args) => {
-        const user = interaction.user;
-        const reason = "Lorem Ipsum dolor si amet dolor sit amet";
+        const user = interaction.options.getUser('target');
+        const reason = interaction.options.getString('reason') === null ? "Lorem ipsum dolor sit amet" : interaction.options.getString('reason');
 
         const embed = new MessageEmbed()
-            .setTitle(`Kick de <@${user.id}>`)
+            .setTitle(`Kick de ${user.username}#${user.tag}!`)
             .setDescription("Etes vous sur de vouloir bannir cette utilisateur ?")
             .addFields(
                 { name: "Utilisateur", value: `<@${user.id}>` },
@@ -52,9 +69,7 @@ module.exports = {
 
         const filter = (interaction) => {
             if (interaction.user.id === interaction.user.id) return true;
-            return interaction.reply({
-                content: "Vous ne pouvez pas utiliser ce bouton",
-            });
+            return interaction.reply({ content: 'Vous ne pouvez pas utiliser ce bouton', ephemeral: true });
         };
 
         const collector = interaction.channel.createMessageComponentCollector({
@@ -69,17 +84,17 @@ module.exports = {
                     embed.setDescription("L'utilisateur a été Kick !");
                     response.react("💥");
                     response.react("😱");
-                    return response.editReply({ embeds: [embed] });
+                    return ButtonInteraction.first().update({ embeds: [embed], components: [] });
                 } catch (e) {
                     embed.setDescription(`Il y a eut une erreur ${e}.`);
-                    return response.editReply({ embeds: [embed] });
+                    return ButtonInteraction.first().update({ embeds: [embed], components: [] });
                 }
             } else {
                 embed.setDescription("L'utilisateur n'a pas été Kick !");
                 embed.setColor("#8ac926");
                 response.react("☮️");
                 response.react("👍");
-                return response.edit({ embeds: [embed] });
+                return ButtonInteraction.first().update({ embeds: [embed], components: [] });
             }
         });
     },
