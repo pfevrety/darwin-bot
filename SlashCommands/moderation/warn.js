7848129b751def9c1d21ee32d9b8e289ab2b181b
Fevret <efevret@gmail.com>
2021-08-25 16:59:59 +0200

    ✨ Add warn command

---
+++ SlashCommands/moderation/warn.js
@@ -0,0 +1,60 @@
+const {
+    MessageEmbed,
+    MessageActionRow,
+    MessageButton
+} = require("discord.js");
+
+module.exports = {
+    name: "warn",
+    type: 1,
+    description: "clear messages",
+    options: [
+        {
+            name: "target",
+            description: "The warn target",
+            type: 6,
+            required: true,
+            choices: []
+        },
+        {
+            name: "reason",
+            description: "The warn reason",
+            type: 3,
+            required: false,
+            choices: []
+        }
+    ],
+
+    /**
+     *
+     * @param {CommandInteraction} interaction
+     * @param {String[]} args
+     */
+    run: async (interaction, args) => {
+        const user = interaction.options.getUser("target");
+        const reason = interaction.options.getString("reason");
+
+
+        const embed = new MessageEmbed()
+            .setTitle(`${user.username} a été warn!`)
+            .setDescription(`Un utilisateur a été warn!`)
+            .addFields(
+                { name: "Utilisateur", value: "<@" + user + ">"},
+                { name: "Raison", value: `\`\`${reason ? reason : "Non définie"}\`\`` },
+                { name: "Nombre de warn au total", value: "``" + "2" + " warns``"},
+                { name: "Autheur du warn", value: "<@" + interaction.user + ">"}
+            )
+            .setThumbnail(user.avatarURL())
+            .setColor("#f3f3f3")
+            .setTimestamp()
+            .setFooter(`Darwin - pfevrety#1908`);
+
+        const response = await interaction.channel.send({
+            embeds: [embed]
+        });
+
+        response.react("🥶");
+        response.react("😱");
+        return response.react("💥");
+    },
+};
