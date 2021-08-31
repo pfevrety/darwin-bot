a8c3b20d3029c929a70556c4eac505b91e9e750f
Fevret <efevret@gmail.Com>
2021-08-31 16:07:22 +0200

    ✨ Create /level

---
+++ SlashCommands/level/level.js
@@ -0,0 +1,61 @@
+const canvacord = require("canvacord");
+const { MessageAttachment } = require("discord.js");
+
+module.exports = {
+    name: "level",
+    type: 1,
+    description: "clear messages",
+    options: [
+        {
+            name: "author",
+            description: "the level of the user",
+            type: 6,
+            required: false,
+            choices: [],
+        },
+    ],
+
+    /**
+     *
+     * @param {CommandInteraction} interaction
+     * @param {String[]} args
+     */
+    run: async (interaction, args) => {
+        const user =
+            interaction.options.getMember("author") || interaction.member;
+
+        let level = await interaction.client.Levels.fetch(
+            user.user.id,
+            interaction.guild.id,
+            true
+        );
+
+        if (!level) {
+            await interaction.client.Levels.createUser(
+                user.user.id,
+                interaction.guild.id
+            );
+            level = await interaction.client.Levels.fetch(
+                user.user.id,
+                interaction.guild.id,
+                true
+            );
+        }
+
+        const rank = new canvacord.Rank()
+            .setAvatar(user.user.displayAvatarURL({ format: "png", size: 512 }))
+            .setCurrentXP(level.xp) // Current User Xp
+            .setRequiredXP(user.client.Levels.xpFor(level.level + 1)) // We calculate the required Xp for the next level
+            .setRank(level.position) // Position of the user on the leaderboard
+            .setLevel(level.level) // Current Level of the user
+            .setStatus(user.presence.status) // Current Status of the user
+            .setProgressBar("#FFFFFF")
+            .setUsername(user.user.username)
+            .setDiscriminator(user.user.discriminator);
+
+        rank.build().then((buffer) => {
+            const attachment = new MessageAttachment(buffer, "rank.png");
+            interaction.followUp({ files: [attachment] });
+        });
+    },
+};
