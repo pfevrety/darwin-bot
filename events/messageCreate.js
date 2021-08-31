3d17684a38a9d89403993e7aca9f2af8ad25a508
Fevret <efevret@gmail.Com>
2021-08-31 16:04:17 +0200

    ✨ Add xp system

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,20 +1,43 @@
 const client = require("../index");
 
 client.on("messageCreate", async (message) => {
-    if (
-        message.author.bot ||
-        !message.guild ||
-        !message.content.toLowerCase().startsWith(client.config.prefix)
-    )
-        return;
+    if (message.author.bot || !message.guild) return;
 
-    const [cmd, ...args] = message.content
-        .slice(client.config.prefix.length)
-        .trim()
-        .split(" ");
+    let hasLeveledUp;
+    try {
+        hasLeveledUp = await message.client.Levels.appendXp(
+            message.author.id,
+            message.guild.id,
+            10
+        );
+    } catch {
+        message.client.Levels.createUser(message.author.id, message.guild.id);
+        hasLeveledUp = await message.client.Levels.appendXp(
+            message.author.id,
+            message.guild.id,
+            5
+        ).catch((e) => {
+            console.log(e);
+        });
+    }
 
-    const command = client.commands.get(cmd.toLowerCase()) || client.commands.find(c => c.aliases?.includes(cmd.toLowerCase()));
+    if (hasLeveledUp) {
+        const user = await message.client.Levels.fetch(
+            message.author.id,
+            message.guild.id
+        );
 
-    if (!command) return;
-    await command.run(client, message, args);
+        message.reply(
+            `Felicitation ${message.author} vous venez de passer niveau ${user.level}. Pour consulter votre xp faite /level`
+        );
+    }
+    // const [cmd, ...args] = message.content
+    //     .slice(client.config.prefix.length)
+    //     .trim()
+    //     .split(" ");
+
+    // const command = client.commands.get(cmd.toLowerCase()) || client.commands.find(c => c.aliases?.includes(cmd.toLowerCase()));
+
+    // if (!command) return;
+    // await command.run(client, message, args);
 });
