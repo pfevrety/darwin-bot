d2efb3932f0bca919bb4acffa3ce27913967218f
Fevret <efevret@gmail.Com>
2021-08-31 16:04:51 +0200

    ✨Configure discord-xp

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const { Client, Collection } = require("discord.js");
+const Levels = require("discord-xp");
 
 const client = new Client({
     intents: 32767,
@@ -9,7 +10,9 @@ module.exports = client;
 client.commands = new Collection();
 client.slashCommands = new Collection();
 client.config = require("./config.json");
+client.Levels = Levels;
 
+client.Levels.setURL(client.config.mongoURL);
 // Initializing the project
 require("./handler")(client);
 
