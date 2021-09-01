7e9fe373caeff25e3718b105e82ae2b26cc7ebc4
Fevret <efevret@gmail.Com>
2021-09-01 13:42:49 +0200

    ✨ Create addxp command

---
+++ SlashCommands/level/addxp.js
@@ -0,0 +1,39 @@
+module.exports = {
+    name: "addxp",
+    type: 1,
+    description: "add xp",
+    options: [
+        {
+            name: "target",
+            description: "add xp to this user",
+            type: 6,
+            required: true,
+            choices: [],
+        },
+        {
+            name: "amount",
+            description: "The amount",
+            type: 4,
+            required: true,
+            choices: [],
+        },
+
+    ],
+
+    /**
+     *
+     * @param {CommandInteraction} interaction
+     */
+    run: async (interaction ) => {
+        const user = interaction.options.getUser("target");
+        const amount = interaction.options.getInteger("amount");
+
+        let level = await interaction.client.Levels.appendXp(
+            user.id,
+            interaction.guild.id,
+            amount
+        );
+
+        interaction.followUp({ content: "L'xp a été ajouter"});
+    },
+};
