c68c31effe34e7af3cce749d39ca1fa4475b2925
Fevret <efevret@gmail.Com>
2021-09-02 21:40:18 +0200

    Update index.js

--- handler/index.js
+++ handler/index.js
@@ -44,47 +44,12 @@ module.exports = async (client) => {
     });
     client.on("ready", async () => {
         // Register for a single guild
-        const guild = client.guilds.cache.get(serverID);
-        // await guild.commands.set(arrayOfSlashCommands).then((cmd) => {
-        //     const getRoles = (commandName) => {
-        //         const permissions = arrayOfSlashCommands.find(
-        //             (x) => x.name === commandName
-        //         ).userPermissions;
+        await client.guilds.cache
+            .get(serverID)
+            .commands.set(arrayOfSlashCommands);
 
-        //         if (!permissions) return null;
-        //         return guild.roles.cache.filter(
-        //             (x) => x.permissions.has(permissions) && !x.managed
-        //         );
-        //     };
-
-        //     // const fullPermissions = cmd.reduce((accumulator, x) => {
-        //     //     const roles = getRoles(x.name);
-        //     //     if (!roles) return accumulator;
-
-        //     //     const permissions = roles.reduce((a, v) => {
-        //     //         return [
-        //     //             ...a,
-        //     //             {
-        //     //                 id: v.id,
-        //     //                 type: "ROLE",
-        //     //                 permission: true,
-        //     //             },
-        //     //         ];
-        //     //     }, []);
-
-        //     //     return [
-        //     //         ...accumulator,
-        //     //         {
-        //     //             id: x.id,
-        //     //             permissions,
-        //     //         },
-        //     //     ];
-        //     // }, []);
-
-        //     guild.commands.permissions.set({ fullPermissions });
-        // });
         // Register for all the guilds the bot is in
-        await client.application.commands.set(arrayOfSlashCommands);
+        // await client.application.commands.set(arrayOfSlashCommands);
     });
 
     // mongoose
